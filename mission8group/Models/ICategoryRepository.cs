a0b6443491548339ccd4da4175729604d8e87572
Caleb Nimmer <cnimmer@byu.edu>
2025-02-28 17:27:27 -0700

    These are the controllers

---
+++ mission8group/Models/ICategoryRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace mission8group.Models
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        // Add any category-specific methods if needed in the future.
+    }
+}
